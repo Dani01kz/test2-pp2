309ea947bc46030d2e6f96db92d8c17fe9559843
Dani01kz <dkanalbay06@gmail.com>
2024-01-24 12:45:12 +0600

    opr

---
+++ lab 2/Operators/examples/ex.py
@@ -0,0 +1,169 @@
+#1
+print(10 + 5)
+print("")
+#2 	Addition
+x = 5
+y = 3
+
+print(x + y)
+print("")
+#3 Subtraction
+x = 5
+y = 3
+
+print(x - y)
+#4 Multiplication
+x = 5
+y = 3
+
+print(x * y)
+print("")
+#5 Division
+x = 12
+y = 3
+
+print(x / y)
+print("")
+#6 Modulus
+x = 5
+y = 2
+
+print(x % y)
+print("")
+#7 Exponentiation
+x = 2
+y = 5
+
+print(x ** y) #same as 2*2*2*2*2
+print("")
+#8 Floor division
+x = 15
+y = 2
+
+print(x // y) #the floor division // rounds the result down to the nearest whole number
+print("")
+#9
+x = 5
+
+x += 3
+
+print(x)
+
+#10
+x = 5
+
+x -= 3
+
+print(x)
+#11
+x = 5
+
+x *= 3
+
+print(x)
+#12
+x = 5
+
+x /= 3
+
+print(x)
+#13
+x = 5
+
+x%=3
+
+print(x)
+#14
+x = 5
+
+x//=3
+
+print(x)
+#15
+x = 5
+
+x **= 3
+
+print(x)
+#16
+x = 5
+
+x &= 3
+
+print(x)
+#17
+x = 5
+
+x |= 3
+
+print(x)
+#18
+x = 5
+
+x ^= 3
+
+print(x)# Ans:3
+#19
+x = 5
+
+x >>= 3
+
+print(x) #Ans:0
+#20
+x = 5
+
+x <<= 3
+
+print(x) #x=40
+#21
+x = 5
+
+print(not(x > 3 and x < 10))
+
+# returns False because not is used to reverse the result
+#21
+print((6 + 3) - (6 + 3))
+#22
+print(100 + 5 * 3)
+#23
+#~	NOT	Inverts all the bits	
+~x
+#24
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+
+# returns True because z is the same object as x
+
+print(x is y)
+
+# returns False because x is not the same object as y, even if they have the same content
+
+print(x == y)
+
+# to demonstrate the difference betweeen "is" and "==": this comparison returns True because x is equal to y
+#25
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is not z)
+
+# returns False because z is the same object as x
+
+print(x is not y)
+
+# returns True because x is not the same object as y, even if they have the same content
+
+print(x != y)
+
+# to demonstrate the difference betweeen "is not" and "!=": this comparison returns False because x is equal to y
+#26
+print(5 + 4 - 7 + 3)
+
+
+
+
+
